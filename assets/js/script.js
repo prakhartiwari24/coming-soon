b787c5d06a64cce41edd74c1a2b85a963fd529d8
prakhar tiwari <prakhartiwari20@gmail.com>
2021-07-03 01:13:49 +0530

    added typewriter effect

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    strings: ["Career", "Startup","Funding", "Growth", "SEO", "Personal", "Networking", "Tech" ,"Marketing"],
+    autoStart: true,    
+    loop: true
+});
+
